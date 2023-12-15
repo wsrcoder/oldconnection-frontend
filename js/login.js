5225886776d87011643239d93e40e262097e0f84
Wellington de Sousa Reis <wsr.coder@gmail.com>
2023-12-14 22:04:19 -0300

    criado validações para o login

--- js/login.js
+++ js/login.js
@@ -14,14 +14,40 @@ async function obterUsuarios()
 
 async function on_login()
 {
-   const email = document.getElementById('email')
-   const senha = document.getElementById('senha')
+    const email = document.getElementById('email')
+    const senha = document.getElementById('password')
+    const max_password_digits = 3
+
+    
+    if(email.value === "")
+    {
+            alert("Campo do email não pode ser vazio.")
+            return
+    }
+
+    if(senha.value === "")
+    {
+            alert("campo senha não pode ser vazio.")
+            return
+    }
+
+    if(!is_valid_email(email.value))
+    {
+        alert("Informe um email valido, por favor.")
+        return
+    }
+
+    if(! is_password_min_digits(password.value, max_password_digits))
+    {
+        alert("Uma senha deve ter no minimo " + max_password_digits + " digitos.")
+        return
+    }
 
 
-   alert(email.value)
     const usuarios = await obterUsuarios()
     let usuario
     let login_sucess = false;
+
     for(var i=0; i < usuarios.length; i++)
     {
 
@@ -68,4 +94,28 @@ async function on_login()
 function btn_responsavel()
 {
     window.location.replace("./CadastroResponsavel.html")
+}
+
+function is_valid_email(email)
+{
+    const email_regex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-z]{2,}$/
+    )
+
+    if(email_regex.test(email))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+function is_password_min_digits(password, min_digits)
+{
+    if(password.length >= min_digits)
+    {
+        return true;
+    }
+
+    return false;
 }
